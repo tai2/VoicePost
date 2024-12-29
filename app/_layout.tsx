f30f0bd4b772ef7fce153bb3f48bfc13a769cdb4
tai2 <hello@tai2.net>
2024-12-29 18:35:06 +0900

    Change the title

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,8 +3,18 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="settings" />
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "声のポスト",
+        }}
+      />
+      <Stack.Screen
+        name="settings"
+        options={{
+          title: "設定",
+        }}
+      />
     </Stack>
   );
 }
