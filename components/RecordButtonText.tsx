a4e1d0b10d5cbb1ffc041734cc73dc1005362a9b
tai2 <hello@tai2.net>
2025-01-13 12:19:59 +0900

    Add icon to text button

--- components/RecordButtonText.tsx
+++ components/RecordButtonText.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Text, TextStyle } from "react-native";
 import { Pressable, StyleProp, ViewStyle } from "react-native";
+import Foundation from "@expo/vector-icons/Foundation";
 
 type Props = {
   isRecording: boolean;
@@ -11,8 +12,10 @@ type Props = {
 export const RecordButtonText = ({ isRecording, onStop, onStart }: Props) => {
   const pressableStyle: StyleProp<ViewStyle> = {
     width: 160,
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    gap: 5,
     padding: 15,
     borderRadius: 10,
   };
@@ -29,6 +32,7 @@ export const RecordButtonText = ({ isRecording, onStop, onStart }: Props) => {
           accessibilityLabel="録音を停止する"
           onPress={onStop}
         >
+          <Foundation name="stop" size={24} color="white" />
           <Text style={textStyle}>録音停止</Text>
         </Pressable>
       ) : (
@@ -37,6 +41,7 @@ export const RecordButtonText = ({ isRecording, onStop, onStart }: Props) => {
           accessibilityLabel="録音を開始する"
           onPress={onStart}
         >
+          <Foundation name="record" size={24} color="white" />
           <Text style={textStyle}>録音開始</Text>
         </Pressable>
       )}
