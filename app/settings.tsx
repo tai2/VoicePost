6ded61908ec44ee8c94d4bbdd0cd711e02d44365
tai2 <hello@tai2.net>
2025-01-18 21:30:26 +0900

    Style settings

--- app/settings.tsx
+++ app/settings.tsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Typography } from "@/constants/Typography";
+import { Spacing } from "@/constants/Spacing";
+import { Colors } from "@/constants/Colors";
+import { Borders } from "@/constants/Borders";
 
 export default function Settings() {
   const [preserveDuration, setPreserveDuration] = useState<string | undefined>(
@@ -27,21 +31,28 @@ export default function Settings() {
   };
 
   return (
-    <View>
-      <Text>ギガファイル便</Text>
-      <Text>保存期限</Text>
-      <Picker
-        selectedValue={preserveDuration}
-        onValueChange={handlePreserveDurationChange}
+    <View style={{ padding: Spacing[6] }}>
+      <View
+        style={{
+          padding: Spacing[2],
+          backgroundColor: Colors.zinc50,
+          ...Borders.roundedLg,
+        }}
       >
-        <Picker.Item label="3日" value="3" />
-        <Picker.Item label="5日" value="5" />
-        <Picker.Item label="7日" value="7" />
-        <Picker.Item label="14日" value="14" />
-        <Picker.Item label="30日" value="30" />
-        <Picker.Item label="60日" value="60" />
-        <Picker.Item label="100日" value="100" />
-      </Picker>
+        <Text style={{ ...Typography.textXl }}>保存期限</Text>
+        <Picker
+          selectedValue={preserveDuration}
+          onValueChange={handlePreserveDurationChange}
+        >
+          <Picker.Item label="3日" value="3" />
+          <Picker.Item label="5日" value="5" />
+          <Picker.Item label="7日" value="7" />
+          <Picker.Item label="14日" value="14" />
+          <Picker.Item label="30日" value="30" />
+          <Picker.Item label="60日" value="60" />
+          <Picker.Item label="100日" value="100" />
+        </Picker>
+      </View>
     </View>
   );
 }
