8e101b109529ddb79e4460e161e5e648cad0184b
tai2 <hello@tai2.net>
2025-01-21 19:50:14 +0900

    Fix a test case

--- lib/getRecordedFilename.test.ts
+++ lib/getRecordedFilename.test.ts
@@ -4,8 +4,8 @@ describe("getRecordedFilename", () => {
   describe("given now is 2021-01-01 01:02:03", () => {
     const now = new Date("2021-01-01T01:02:03");
 
-    it("should return recording_20210101_010203.mp3", () => {
-      expect(getRecordedFilename(now)).toBe("recording_20210101_010203.mp3");
+    it("should return recording_20210101_010203.m4a", () => {
+      expect(getRecordedFilename(now)).toBe("recording_20210101_010203.m4a");
     });
   });
 });
