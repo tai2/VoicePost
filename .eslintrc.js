a0b296afdea2a21c7843026d92c6388a0f10a7a0
tai2 <hello@tai2.net>
2025-01-29 20:44:54 +0900

    Resolve lint warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
-  ignorePatterns: ['/dist/*'],
+  extends: "expo",
+  ignorePatterns: ["/dist/*"],
+  overrides: [
+    {
+      files: ["jest-setup.js"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
