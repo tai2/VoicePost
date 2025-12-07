4b69820a5c41cddb03bf346ca0dfd952ebab3d3c
tai2 <hello@tai2.net>
2025-12-07 17:35:24 +0900

    Import from legacy filesystem API

--- lib/gigafile/upload.ts
+++ lib/gigafile/upload.ts
@@ -1,4 +1,4 @@
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 import * as Crypto from "expo-crypto";
 import * as Sentry from "@sentry/react-native";
 
